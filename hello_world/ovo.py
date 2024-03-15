8fa77f20727f289a310883e84f79728657bb01c6
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-03-15 18:49:38 +0000

    --wip--

--- hello_world/ovo.py
+++ hello_world/ovo.py
@@ -17,7 +17,6 @@ def current_milli_time(time: datetime) -> str:
 
 
 def load():
-
     # Create a Systems Manager client
     ssm = boto3.client('ssm', region_name='eu-west-1')
 
@@ -32,61 +31,63 @@ def load():
     client = OVOEnergy()
     authenticated = asyncio.run(client.authenticate(user, password))
 
-
     if authenticated:
         print("authenticated")
 
-        start_date = date(2024, 2, 8)
-        end_date = date(2024, 2, 16)
+        single_date = date(2024, 3, 14)
         session = boto3.Session()
-        write_client = session.client('timestream-write', config=Config(read_timeout=20, max_pool_connections = 5000, retries={'max_attempts': 10}))
+        write_client = session.client('timestream-write', config=Config(read_timeout=20, max_pool_connections=5000,
+                                                                        retries={'max_attempts': 10}))
 
-        for single_date in daterange(start_date, end_date):
-            string_date = single_date.strftime("%Y-%m-%d")
-            half_hourly_usage = asyncio.run(client.get_half_hourly_usage(string_date))
-            electricity = half_hourly_usage.electricity
+        string_date = single_date.strftime("%Y-%m-%d")
+        half_hourly_usage = asyncio.run(client.get_half_hourly_usage(string_date))
+        electricity = half_hourly_usage.electricity
 
-            dimensions = [
-                {'Name': 'supplier', 'Value': 'ovo'},
-                {'Name': 'postcode', 'Value': 'BN16HL'},
-            ]
-            records = []
-            for half_hour in electricity:
-                record = {
-                    'Dimensions': dimensions,
-                    'MeasureName': 'consumption',
-                    'MeasureValue': str(half_hour.consumption),
-                    'MeasureValueType': 'DOUBLE',
-                    'Time': current_milli_time(half_hour.interval.start)
-                }
-                records.append(record)
-                if len(records) == 100:
-                    try:
-                        result = write_client.write_records(
-                            DatabaseName="home",
-                            TableName="electricity",
-                            Records=records,
-                            CommonAttributes={})
+        dimensions = [
+            {'Name': 'supplier', 'Value': 'ovo'},
+            {'Name': 'postcode', 'Value': 'BN16HL'},
+        ]
+        records = []
+        for half_hour in electricity:
+            start_time = half_hour.interval.start
+            if start_time < (datetime.now() - timedelta(days=1)):
+                continue
+            record = {
+                'Dimensions': dimensions,
+                'MeasureName': 'consumption',
+                'MeasureValue': str(half_hour.consumption),
+                'MeasureValueType': 'DOUBLE',
+                'Time': current_milli_time(half_hour.interval.start)
+            }
+            records.append(record)
+            if len(records) == 100:
+                try:
+                    result = write_client.write_records(
+                        DatabaseName="home",
+                        TableName="electricity",
+                        Records=records,
+                        CommonAttributes={})
 
-                        # reset the batch
-                        records = []
-                        print("WriteRecords Status: [%s]" % result['ResponseMetadata']['HTTPStatusCode'])
-                    except client.exceptions.RejectedRecordsException as err:
-                        print("Error:", err)
-                    except Exception as err:
-                        print("Error:", err)
+                    # reset the batch
+                    records = []
+                    print("WriteRecords Status: [%s]" % result['ResponseMetadata']['HTTPStatusCode'])
+                except client.exceptions.RejectedRecordsException as err:
+                    print("Error:", err)
+                except Exception as err:
+                    print("Error:", err)
 
+        if len(records) > 0:
             result = write_client.write_records(
                 DatabaseName="home",
                 TableName="electricity",
                 Records=records,
                 CommonAttributes={})
 
+
     return {
         'statusCode': 200,
         'body': json.dumps('written')
     }
-
 
 if __name__ == "__main__":
     load()
